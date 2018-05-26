52bfe3678571fb983082562ca653b479d085ff64
JP Hashagen <jayp33e@gmail.com>
2018-05-26 14:32:48 +0200

    From scratch - Green container wit AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -9,33 +9,14 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hello Rectangle',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Hello Rectangle'),
         ),
-        body: HelloRectangle(),
+        body: Container(
+          color: Colors.greenAccent,
+        ),
       ),
     ),
   );
-}
-
-class HelloRectangle extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 400.0,
-        width: 300.0,
-        child: Center(
-          child: Text(
-            'Hello!',
-            style: TextStyle(fontSize: 40.0),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
-    );
-  }
 }
