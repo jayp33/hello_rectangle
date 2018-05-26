e49589d76871fdfdc6d3102ed7fe28a168542533
JP Hashagen <jayp33e@gmail.com>
2018-05-26 15:03:14 +0200

    Center widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,12 @@ import 'package:flutter/material.dart';
 class HelloRectangle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.greenAccent,
-      height: 400.0,
-      width: 300.0,
+    return new Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+      ),
     );
   }
 }
