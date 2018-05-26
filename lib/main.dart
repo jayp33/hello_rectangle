e4f3273ad455c2f355b526324a94f95a7e2ace6a
JP Hashagen <jayp33e@gmail.com>
2018-05-26 15:07:51 +0200

    Remove obsolote new keywords

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,12 @@ import 'package:flutter/material.dart';
 class HelloRectangle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(
+    return Center(
       child: Container(
         color: Colors.greenAccent,
         height: 400.0,
         width: 300.0,
-        child:
-            new Center(child: Text('Hello!', style: TextStyle(fontSize: 40.0))),
+        child: Center(child: Text('Hello!', style: TextStyle(fontSize: 40.0))),
       ),
     );
   }
